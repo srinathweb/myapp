742614dacf875335d6b6961202b2e20ca541d5da
srinathweb <madusanka525@gmail.com>
2015-03-01 01:38:49 -0800

    test

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#">
+<head>
+  <meta charset="utf-8" />
+  <title>View Source App</title>
+  <link href='https://fonts.googleapis.com/css?family=Exo:700,700italic|Average' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="_/css/mystyle.css">
+  <script type="text/javascript" src="_/js/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="_/js/myscript.js"></script>
+</head>
+<body>
+  <div id="content">
+  <img src="images/vs_monogram.png"alt="View Source Monogram" />
+  <h2>My First App</h2>
+  <p>This is some text to make sure my app is working on Facebook</p>
+  </div><!--content -->
+</body>
+</html>
